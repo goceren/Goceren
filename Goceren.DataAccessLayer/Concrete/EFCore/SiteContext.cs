3133190b2be6af788d1c50800c46971edfb1ef26
goceren <vygoceren@gmail.com>
2020-03-06 18:27:52 +0300

    Updated

--- Goceren.DataAccessLayer/Concrete/EFCore/SiteContext.cs
+++ Goceren.DataAccessLayer/Concrete/EFCore/SiteContext.cs
@@ -10,7 +10,7 @@ namespace Goceren.DataAccessLayer
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@" ");
+            optionsBuilder.UseSqlServer(@"");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
